d7c2d8d2d748269d61a3dd0c7c45e70dada1010d
Ray Xi <rayxi@Rays-MacBook-Air.local>
2022-12-17 06:57:30 -0800

    gpt fixed the cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
 import * as THREE from "three";
 class Cube extends Component {
+    constructor() {
+      super();
+      this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      this.renderer = new THREE.WebGLRenderer();
+      this.geometry = new THREE.BoxGeometry(1, 1, 1);
+      this.material = new THREE.MeshStandardMaterial({ color: 0x7e31eb });
+      this.cube = new THREE.Mesh(this.geometry, this.material);
+      this.light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+    }
+  
     componentDidMount() {
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        this.mount.appendChild( renderer.domElement );
-      
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshStandardMaterial( { color: 0x7e31eb } );
-        var cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
-        const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-        scene.add( light );
-camera.position.z = 2;
-var animate = function () {
-            requestAnimationFrame( animate );
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            cube.rotation.z += 0.01;
-            renderer.render( scene, camera );
-        };
-        animate();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.mount.appendChild(this.renderer.domElement);
+      this.scene.add(this.cube);
+      this.scene.add(this.light);
+      this.camera.position.z = 2;
+      this.animate();
     }
+  
+    animate() {
+      requestAnimationFrame(this.animate.bind(this));
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
+      this.cube.rotation.z += 0.01;
+      this.renderer.render(this.scene, this.camera);
+    }
+  
     render() {
-        return (
-            <div className="" ref={ref => (this.mount = ref)} />
-        )
+      return <div className="cube" ref={(ref) => (this.mount = ref)} />;
     }
-}
+  }
+  
 export default Cube;
