0843afec92778d4f46608bd5dee6d9929232ea3a
Rui Xi <xr@berkeley.edu>
2023-02-01 01:33:30 -0800

    update

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <title>The Rock (1996)</title>
         <meta property="og:title" content="The Rock" />
         <meta property="og:type" content="video.movie" />
-        <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+        <meta property="og:url" content="https://exoreray.github.io/" />
         <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
       </Helmet>
       <BrowserRouter>
